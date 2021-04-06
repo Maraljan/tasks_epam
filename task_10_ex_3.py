03e692d2b39b01aef825510759940766afbeafef
Ashirova Maral <ashirova310197@gmail.com>
2021-04-06 16:34:48 +0300

    implemented File `data/students.csv` stores information about students in CSV format

--- task_10_ex_3.py
+++ task_10_ex_3.py
@@ -29,13 +29,54 @@ Brenda Silva,30,7.53
 Lindsey Cummings,18,6.88
 Raymond Soileau,18,7.27
 """
+import csv
 
 
-def get_top_performers():
-    # Your code
-    pass
+class Student:
+    def __init__(self, name: str, age: int, mark: float):
+        self.name = name
+        self.age = age
+        self.mark = mark
+
+    def __repr__(self):
+        return f'({self.name}, {self.age}, {self.mark})'
 
 
-def write_students_age_desc():
-    # Your code
-    pass
+def get_list_students(file_path: str):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        list_students = []
+        for one_student in reader:
+            student = Student(
+                name=one_student['student name'],
+                age=int(one_student['age']),
+                mark=float(one_student['average mark']),
+            )
+            list_students.append(student)
+        return list_students
+
+
+# print(get_list_students('students.csv'))
+
+
+def get_top_performers(file_path, number_of_top_students=5):
+    top_students = get_list_students(file_path)
+    sorted_students = sorted(top_students, key=lambda x: x.mark, reverse=True)
+    return [student.name for student in sorted_students[:number_of_top_students]]
+
+
+# print(get_top_performers('students.csv', 5))
+
+
+def write_students_age_desc(file_path, output_file):
+    top_students = get_list_students(file_path)
+    sorted_students = sorted(top_students, key=lambda x: x.age, reverse=True)
+
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['student name', 'age', 'average mark'])
+        writer.writeheader()
+        for student in sorted_students:
+            writer.writerow({'student name': student.name, 'age': student.age, 'average mark': student.mark})
+
+
+print(write_students_age_desc('students.csv', 'new_students.csv'))
